55c62e27086d3869ff667be1047f8f3fe5ecc55d
Sett353 <arenasvelezs@gmail.com>
2026-07-29 10:52:28 -0500

    Actualización de metodos

--- main.py
+++ main.py
@@ -6,17 +6,20 @@ app = FastAPI()
 
 crear_tabla()
 
+tasa_usd=3205.87
+
 @app.get("/")
 def inicio():
     return {"mensaje": "API funcionando"}
 
 @app.post("/productos")
 def crear_producto(producto: Producto):
+    precio_usd= producto.precio_cop/tasa_usd
     conn = get_connection()
     cursor = conn.cursor()
     cursor.execute("INSERT INTO productos"\
         "(referencia, nombre, precio_cop, precio_usd, estado) VALUES (?, ?, ?, ?, ?)", 
-        (producto.referencia, producto.nombre, producto.precio_cop, producto.precio_usd, producto.estado))
+        (producto.referencia, producto.nombre, producto.precio_cop, precio_usd, producto.estado))
     conn.commit()
     conn.close()
     return {"mensaje": "Producto creado exitosamente"}
@@ -29,3 +32,39 @@ def listar_productos():
     ).fetchall()
     conn.close()
     return [dict(x) for x in productos]
+
+@app.get ("/productos/{id}")
+def buscar (id: int):
+    conn = get_connection()
+    producto= conn.execute(
+        "SELECT * FROM productos WHERE id = ?",
+        (id,)
+    ).fetchone()
+    conn.close()
+    if producto:
+        return dict(producto)
+    return {"mensaje": "No existe"}
+
+@app.put("/productos/{id}")
+def actualizar (id:int, producto:Producto):
+    precio_usd= producto.precio_cop/tasa_usd
+    conn = get_connection()
+    conn.execute(
+        "UPDATE productos SET referencia=?,nombre=?,precio_cop=?,precio_usd=?,estado=? WHERE id=?",
+        (producto.referencia,producto.nombre,producto.precio_cop,precio_usd,producto.estado,id)
+    )
+    conn.commit()
+    conn.close()
+    return {"mensaje": "Actualizado correctamente"}
+
+@app.delete("/productos/{id}")
+def eliminar(id: int):
+
+    conn = get_connection()
+    conn.execute(
+    "DELETE FROM productos WHERE id=?",
+    (id,)
+    )
+    conn.commit()
+    conn.close()
+    return {"mensaje": "Eliminado"}
--- models.py
+++ models.py
@@ -1,9 +1,7 @@
 from pydantic import BaseModel
 
 class Producto(BaseModel):
-    referencia: str 
-    nombre: str 
+    referencia: str
+    nombre: str
     precio_cop: float
-    precio_usd: float
     estado: bool
-    
